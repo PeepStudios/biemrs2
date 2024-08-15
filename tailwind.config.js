9fe7c58ef03cdf584e859c7bb8eb3063038f594f
Peep.hq <130054144+PeepStudios@users.noreply.github.com>
2024-08-15 03:26:29 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{html,js,jsx}"],
+  theme: {
+    background: {
+      hero_bg: "/images/8-Ways-to-Better-Secure-Patient-Medical-Records.jpg"
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+};
